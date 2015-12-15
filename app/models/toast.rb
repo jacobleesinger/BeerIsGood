3d635f4da93e9e43e7427385284aefcdc2807878
jacobleesinger <jacobleesinger@gmail.com>
2015-12-15 09:26:48 -0800

    associations

--- app/models/toast.rb
+++ app/models/toast.rb
@@ -1,3 +1,4 @@
 class Toast < ActiveRecord::Base
   belongs_to :review
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,12 @@ class User < ActiveRecord::Base
   primary_key: :id
   )
 
+  has_many (
+  :beers,
+  through: :reviews,
+  source: :beer
+  )
+  
   has_many(
   :comments,
   class_name: "Comment",
@@ -28,7 +34,5 @@ class User < ActiveRecord::Base
   )
 
   has_many :toasts
-
-  has_many :beers, through: :reviews, source: :beer
 
 end

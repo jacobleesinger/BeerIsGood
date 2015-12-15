03c4b2d75ff0cef6cf79e07b3a1482dbbe295ed9
jacobleesinger <jacobleesinger@gmail.com>
2015-12-15 08:30:25 -0800

    working on associations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,16 @@
 class Friendship < ActiveRecord::Base
+
+  belongs_to(
+  :user,
+  class_name: "User",
+  foreign_key: :user_id,
+  primary_key: :id
+  )
+
+  belongs_to(
+  :friend,
+  class_name: "User",
+  foreign_key: :friend_id,
+  primary_key: :id
+  )
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
 
   has_many(
+  :friendships,
+  class_name: "Friendship",
+  foreign_key: :user_id,
+  primary_key: :id
+  )
+
+  has_many(
   :reviews,
   class_name: "Review",
   foreign_key: :author_id,

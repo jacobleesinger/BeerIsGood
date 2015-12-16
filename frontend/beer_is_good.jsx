ea131efd702cd1d723d18ec2aa514bcadd7de025
jacobleesinger <jacobleesinger@gmail.com>
2015-12-15 18:02:41 -0800

    phase 1 basic auth complete

--- frontend/beer_is_good.jsx
+++ frontend/beer_is_good.jsx
@@ -1,10 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var LandingPage = require('./components/landing_page');
+
+
+var routes = (
+  <Route path="/" component={}>
+    <IndexRoute component={LandingPage}/>
+  </Route>
+);
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.render(
-    <div>Hi</div>,
-    document.getElementById('root')
-  );
+  ReactDOM.render(<Router>{routes}</Router>, document.getElementById("root"));
 });
---
+++ frontend/components/landing_page.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+
+var LandingPage = React.createClass({
+
+});
+
+module.exports = LandingPage;

7d2d9c817e5d0b5028497f019308512e15b94235
jacobleesinger <jacobleesinger@gmail.com>
2016-03-16 12:46:02 -0700

    refactor comment to functional component

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -35586,8 +35586,6 @@
 	  value: true
 	});
 	
-	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-	
 	var _react = __webpack_require__(20);
 	
 	var _react2 = _interopRequireDefault(_react);
@@ -35600,73 +35598,30 @@
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	var Comment = function Comment(props) {
 	
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	  var author = _user_store2.default.findById(props.comment.author_id);
+	  var url = "/user/" + author.id;
 	
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-	
-	var Comment = (function (_Component) {
-	  _inherits(Comment, _Component);
-	
-	  function Comment(props) {
-	    _classCallCheck(this, Comment);
-	
-	    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(Comment).call(this, props));
-	
-	    _this.state = {
-	      author: _user_store2.default.findById(_this.props.comment.author_id)
-	    };
-	    _this._onChange = _this._onChange.bind(_this);
-	    return _this;
-	  }
-	
-	  _createClass(Comment, [{
-	    key: 'componentDidMount',
-	    value: function componentDidMount() {
-	      this.authorToken = _user_store2.default.addListener(this._onChange);
-	    }
-	  }, {
-	    key: 'componentWillUnmount',
-	    value: function componentWillUnmount() {
-	      this.authorToken.remove();
-	    }
-	  }, {
-	    key: '_onChange',
-	    value: function _onChange() {
-	      this.setState({
-	        author: _user_store2.default.findById(this.props.comment.author_id)
-	      });
-	    }
-	  }, {
-	    key: 'render',
-	    value: function render() {
-	      var url = "/user/" + this.state.author.id;
-	      return _react2.default.createElement(
-	        'div',
-	        { className: 'commentContainer' },
-	        _react2.default.createElement(
-	          'div',
-	          { className: 'commentBody' },
-	          _react2.default.createElement(
-	            _reactRouter.Link,
-	            {
-	              className: 'commentAuthor',
-	              to: url },
-	            this.state.author.username,
-	            ':'
-	          ),
-	          ' ',
-	          this.props.comment.body
-	        )
-	      );
-	    }
-	  }]);
-	
-	  return Comment;
-	})(_react.Component);
-	
-	;
+	  return _react2.default.createElement(
+	    'div',
+	    { className: 'commentContainer' },
+	    _react2.default.createElement(
+	      'div',
+	      { className: 'commentBody' },
+	      _react2.default.createElement(
+	        _reactRouter.Link,
+	        {
+	          className: 'commentAuthor',
+	          to: url },
+	        author.username,
+	        ':'
+	      ),
+	      ' ',
+	      props.comment.body
+	    )
+	  );
+	};
 	
 	exports.default = Comment;
 
--- frontend/components/comment.jsx
+++ frontend/components/comment.jsx
@@ -1,46 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserStore from '../stores/user_store';
 import { Link } from 'react-router';
 
-class Comment extends Component {
+const Comment = (props) => {
 
-  constructor(props) {
-    super(props);
+    let author = UserStore.findById(props.comment.author_id)
+    let url = "/user/" + author.id
 
-    this.state = {
-      author: UserStore.findById(this.props.comment.author_id)
-    };
-    this._onChange = this._onChange.bind(this);
-  }
-
-  componentDidMount() {
-    this.authorToken = UserStore.addListener(this._onChange);
-  }
-
-  componentWillUnmount() {
-    this.authorToken.remove();
-  }
-
-  _onChange() {
-    this.setState({
-      author: UserStore.findById(this.props.comment.author_id)
-    });
-  }
-
-  render() {
-    let url = "/user/" + this.state.author.id
     return (
       <div className="commentContainer">
         <div className ="commentBody">
           <Link
             className="commentAuthor"
-            to={url}>{this.state.author.username}:
-          </Link> {this.props.comment.body}
+            to={ url }>{ author.username }:
+          </Link> { props.comment.body }
         </div>
       </div>
     );
-  }
-
 };
 
 export default Comment;
